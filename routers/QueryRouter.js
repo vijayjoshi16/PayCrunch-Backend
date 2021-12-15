d2d8ed7fa322a558e31a0f4a1be7d5e1fa40005d
vijayjoshi16 <joshivijay324@gmail.com>
2021-12-15 22:25:17 +0530

    Add search API

--- routers/QueryRouter.js
+++ routers/QueryRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const queryRouter = express.Router();
 const expressAsyncHandler = require('express-async-handler');
+const { all } = require('express/lib/application');
 const Video = require('../models/Video');
 
 queryRouter.get(
@@ -10,7 +11,7 @@ queryRouter.get(
             const allVideos = await Video.find({}).sort({publishedAt: -1})
             .skip(parseInt(req.params.skip))
             .limit(parseInt(req.params.limit));
-            console.log(allVideos.length)
+            
             res.status(200).send({
                 message: "Success",
                 videos: allVideos
@@ -21,6 +22,67 @@ queryRouter.get(
                 message:"Some Error Occured"
             })
         }
+    })
+);
+
+queryRouter.post(
+    '/search_videos',
+    expressAsyncHandler(async (req,res)=>{
+        try{
+            const title = req.body.title;
+            const description = req.body.description;
+            const mode = req.body.mode;
+            if(mode=="exact"){
+                //This is the exact mode where we'll only check
+                //for exactly matching title or description
+                const allVideos = await Video.find({
+                    $or: [{title: title}, {description: description}]
+                });
+
+                res.status(200).send({
+                    message:"Success",
+                    videos: allVideos
+                });
+            }else{
+                //This is the smart mode where we'll use
+                //regular expressions to perform advanced search
+
+                //Splitting the title and description space-wise to do word by word analysis
+                const allTitleWords = title.split(" ");
+                const allDescriptionWords = description.split(" ");
+
+                //Preparing regular expressions for the entire string as well as that for each word
+                const titleRegex = [{title: {$regex: `${title}+`, $options:"i"}}];
+                const descriptionRegex = [{description: {$regex: `${description}+`, $options:"i"}}];
+
+                //inserting regular expression for each word in the array
+                allTitleWords.forEach(keyword=>{
+                    titleRegex.push({title: {$regex: `${keyword}+`, $options:"i"}});
+                })
+
+                allDescriptionWords.forEach(keyword=>{
+                    descriptionRegex.push({description: {$regex: `${keyword}+`, $options:"i"}})
+                })
+
+                //Performing the final query with the prepared set of regular expressions
+                const allVideos = await Video.find({
+                    $or: [
+                        ...titleRegex,
+                        ...descriptionRegex
+                    ]
+                });
+                
+                res.status(200).send({
+                    message:"Success",
+                    videos: allVideos
+                });
+            }
+        }catch(err){
+            console.log(err);
+            res.status(500).send({
+                message:"Some Error Occured"
+            })
+        }  
     })
 );
 
