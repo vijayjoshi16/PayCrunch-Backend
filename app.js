1c54bc7b12862798f517b2534222b58d00f429e4
Vijay Joshi <joshivijay324@gmail.com>
2021-12-16 11:16:32 +0530

    Update time interval

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ env.config();
 
 app.use('/query', queryRouter);
 
-setInterval(getVideoFromAPI,10000);
+setInterval(getVideoFromAPI,120000);
 
 //CONNECTING TO MONGODB
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true});
